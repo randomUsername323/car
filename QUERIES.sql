3a4c8cdf136a80af1a44c8af8c9e1f92d37509a1
randomUsername323 <34407645+randomUsername323@users.noreply.github.com>
2017-12-11 23:07:19 -0800

    Update QUERIES.sql

--- QUERIES.sql
+++ QUERIES.sql
@@ -5,23 +5,116 @@ UNION ALL
 SELECT 'Premier' AS CustomerType, firstname, lastname, birthdate, phonenumber, 'N/A' as email FROM person INNER JOIN customer on
 	person.personid = customer.person_personid RIGHT JOIN premier on customer.CUSTOMERID = premier.CUSTOMER_CUSTOMERID;
 
+
+
 /* 2 */
 SELECT customer.customerid, serviceappointment_appdate  AS DateVisit, SUM(totalcostofservice) AS TotalCostForVisit FROM customer
-	INNER JOIN serviceappointment ON customer.CUSTOMERID = serviceappointment.CUSTOMER_CUSTOMERID INNER JOIN maintenancevisitorder ON
-	appdate = maintenancevisitorder.SERVICEAPPOINTMENT_APPDATE GROUP BY customer.customerid, SERVICEAPPOINTMENT_APPDATE;
+	INNER JOIN serviceappointment ON customer.CUSTOMERID = serviceappointment.CUSTOMER_CUSTOMERID
+	INNER JOIN maintenancevisitorder ON appdate = maintenancevisitorder.SERVICEAPPOINTMENT_APPDATE
+    	AND maintenancevisitorder.`ServiceAppointment_Customer_customerID` = serviceappointment.`Customer_customerID`
+    	AND maintenancevisitorder.`ServiceAppointment_Vehicle_VIN` = serviceappointment.`Vehicle_VIN`
+	GROUP BY customer.customerid, SERVICEAPPOINTMENT_APPDATE;
+
+OR
+
+SELECT CUSTOMERID, ServiceAppointment_appDate AS DayOf, SUM(totalcostofservice) AS TotalCost
+FROM Customer
+INNER JOIN MaintenanceVisitOrder
+    ON Customer.customerID = MaintenanceVisitOrder.ServiceAppointment_Customer_customerID
+GROUP BY CUSTOMERID,ServiceAppointment_appDate;
+
+
+
+
+/* 3 */
+SELECT customerID, (SELECT FirstName FROM Person WHERE Person.personID = Customer.Person_PersonID) AS FIRSTNAME,
+(SELECT LastName FROM Person WHERE Person.personID = Customer.Person_PersonID) AS LASTNAME,
+(SELECT SUM(totalcostofservice)FROM MaintenanceVisitOrder WHERE MaintenanceVisitOrder.ServiceAppointment_Customer_customerID = Customer.CUSTOMERID AND YEAR(serviceappointment_appdate) > (YEAR(CURRENT_DATE)-2)) 
+AS TOTAL
+FROM Customer
+WHERE (SELECT SUM(totalcostofservice)FROM MaintenanceVisitOrder WHERE MaintenanceVisitOrder.ServiceAppointment_Customer_customerID = Customer.CUSTOMERID AND YEAR(serviceappointment_appdate) > (YEAR(CURRENT_DATE)-2)) IS NOT NULL
+ORDER BY TOTAL DESC
+LIMIT 3;
+
+/*Another way of doing 3*/
+SELECT customer.customerid, person.`FirstName`, person.`LastName`, SUM(totalcostofservice) AS TotalCostForVisit FROM customer
+	INNER JOIN serviceappointment ON customer.CUSTOMERID = serviceappointment.CUSTOMER_CUSTOMERID
+	INNER JOIN PERSON ON customer.`Person_PersonID` = person.`PersonID`
+	INNER JOIN maintenancevisitorder ON appdate = maintenancevisitorder.SERVICEAPPOINTMENT_APPDATE
+    	AND maintenancevisitorder.`ServiceAppointment_Customer_customerID` = serviceappointment.`Customer_customerID`
+    	AND maintenancevisitorder.`ServiceAppointment_Vehicle_VIN` = serviceappointment.`Vehicle_VIN`
+	WHERE SERVICEAPPOINTMENT_APPDATE BETWEEN DATE_SUB(NOW(), INTERVAL 2 YEAR) and NOW()
+	GROUP BY customer.customerid, person.`FirstName`, person.`LastName`
+	ORDER BY TotalCostForVisit DESC
+	LIMIT 3;
+
+
+
 
 /* 4 */
-SELECT m.EMPLOYEE_EMPLOYEEID from mechanic m INNER JOIN certification c on m.EMPLOYEE_EMPLOYEEID = c.MECHANIC_EMPLOYEE_EMPLOYEEID
-	INNER JOIN skill s on c.SKILL_SKILLNAME = s.SKILLNAME GROUP BY m.EMPLOYEE_EMPLOYEEID HAVING COUNT(s.SKILLNAME) > 2;
+SELECT p.`FirstName`, p.`LastName`, c.`Mechanic_Employee_EmployeeID` from person p INNER JOIN employee e ON
+	p.`PersonID` = e.`Person_PersonID` INNER JOIN mechanic m ON e.`EmployeeID` = m.`Employee_EmployeeID` INNER JOIN certification c
+	on m.`Employee_EmployeeID` = c.`Mechanic_Employee_EmployeeID` INNER JOIN skill s on c.`Skill_skillName` = s.`skillName`
+	GROUP BY p.`FirstName`, p.`LastName`, c.`Mechanic_Employee_EmployeeID` HAVING COUNT(c.`Skill_skillName`) > 2;
+
+
+
 
 /* 6 */
 SELECT sq.packagename, sq.TotalCost, m2.itemname FROM (SELECT packagename, packageid, SUM(cost) AS TotalCost FROM package INNER JOIN maintenanceitem ON
 	package.PACKAGEID = maintenanceitem.PACKAGE_PACKAGEID GROUP BY packagename, packageid) AS sq INNER JOIN maintenanceitem m2
 	ON m2.PACKAGE_PACKAGEID = sq.packageid;
 
+
+/* 7 */
+SELECT DISTINCT m.`Employee_EmployeeID`, p.`FirstName`, p.`LastName` FROM mechanic m
+	INNER JOIN Person p ON m.`Employee_EmployeeID` = p.`PersonID`
+	INNER JOIN MaintenanceVisitOrder mvo ON m.`Employee_EmployeeID` = mvo.`Mechanic_Employee_EmployeeID`
+	INNER JOIN MaintenanceItem mi ON mvo.`MaintenanceItem_maintID` = mi.`maintID`
+	LEFT OUTER JOIN certification c ON c.`Skill_skillName` = mi.`Skill_skillRequired`
+	WHERE c.`Mechanic_Employee_EmployeeID` = m.`Employee_EmployeeID` and c.`Skill_skillName` is NULL;
+
+
+
 /* 8 */
 SELECT firstname, lastname, birthdate, customerid, loyaltypoints FROM person p INNER JOIN customer c
 ON p.PERSONID = c.PERSON_PERSONID INNER JOIN steady s ON c.CUSTOMERID = s.CUSTOMER_CUSTOMERID ORDER BY loyaltypoints DESC;
+
+
+/* 9 */
+SELECT c.`customerID`, p.`FirstName`, p.`LastName`, SUM(mp.`paymentAmount`) - SUM(ServicesUsed.TotalCostOfService) as PaymentMinusServiceUsed FROM customer c
+	INNER JOIN person p ON c.`Person_PersonID` = p.`PersonID`
+	INNER JOIN premier pr ON c.`customerID` = pr.`Customer_customerID`
+	LEFT JOIN (SELECT c2.`customerID`, m2.`TotalCostOfService` FROM customer c2
+    	INNER JOIN person p2 ON c2.`Person_PersonID` = p2.`PersonID`
+    	INNER JOIN premier pr2 ON c2.`customerID` = pr2.`Customer_customerID`
+    	INNER JOIN serviceappointment s2 ON c2.`customerID` = s2.`Customer_customerID`
+    	INNER JOIN maintenancevisitorder m2 ON s2.`Customer_customerID` = m2.`ServiceAppointment_Customer_customerID`
+        	AND s2.`appDate` = m2.`ServiceAppointment_appDate` AND s2.`Vehicle_VIN` = m2.`ServiceAppointment_Vehicle_VIN`
+    	WHERE s2.`appDate` BETWEEN  DATE_SUB(NOW(), INTERVAL 3 YEAR) AND NOW()
+	) as ServicesUsed ON c.`customerID` = ServicesUsed.customerID
+	LEFT OUTER JOIN MonthlyPayment mp ON pr.`Customer_customerID` = mp.`Premier_Customer_customerID`
+	WHERE mp.`paymentDate` BETWEEN  DATE_SUB(NOW(), INTERVAL 3 YEAR) AND NOW()
+	GROUP BY c.`customerID`, p.`FirstName`, p.`LastName`
+	ORDER BY SUM(mp.`paymentAmount`) - SUM(ServicesUsed.TotalCostOfService) DESC;
+
+
+
+/* 10 */
+SELECT c.`customerID`, SUM(m.`TotalCostOfService`) SpendingTotal
+	FROM customer c INNER JOIN serviceappointment s ON c.`customerID` = s.`Customer_customerID`
+	INNER JOIN maintenancevisitorder m ON s.`Customer_customerID` = m.`ServiceAppointment_Customer_customerID`
+	AND s.`appDate` = m.`ServiceAppointment_appDate` AND s.`Vehicle_VIN` = m.`ServiceAppointment_Vehicle_VIN`
+	INNER JOIN maintenanceitem mt ON m.`MaintenanceItem_maintID` = mt.`maintID` WHERE c.`customerID` IN
+	(SELECT c.`customerID` FROM customer c INNER JOIN steady s ON c.`customerID` = s.`Customer_customerID`
+	WHERE m.`ServiceAppointment_appDate` >= '2016-01-01' AND m.`ServiceAppointment_appDate` <= '2016-12-31'
+	GROUP BY c.`customerID`);
+
+/*11*/
+SELECT Customer_customerID, (calculatedAnnualFee*12) AS SUM_PAYMENTS FROM Premier
+ORDER BY SUM_PAYMENTS;
+
+
 
 /* 15 */
 SELECT e.EMPLOYEEID, 'Technician' AS Occupation FROM employee e INNER JOIN technician t ON e.EMPLOYEEID = t.employee_employeeid
@@ -29,14 +122,25 @@ SELECT e.EMPLOYEEID, 'Technician' AS Occupation FROM employee e INNER JOIN techn
 UNION ALL
 SELECT e.EMPLOYEEID, 'Mechanic' AS Occupation FROM employee e INNER JOIN mechanic m ON e.EMPLOYEEID = m.employee_employeeid
 	WHERE e.EMPLOYEEID IN (SELECT e.EMPLOYEEID FROM employee e INNER JOIN mechanic m ON e.EMPLOYEEID = m.EMPLOYEE_EMPLOYEEID);
+
+
 	
 /* 17 */
 SELECT itemname FROM maintenanceitem WHERE package_packageid IS NULL;
+
+
+
+
+/* 16 */
+SELECT s.`intervalMile`, s.`Package_PackageID` FROM vehicle v INNER JOIN serviceinterval s ON v.`VIN` = s.`Vehicle_VIN` INNER JOIN
+	package p ON p.`PackageID` = s.`S_INTERVAL_ID`;
 
 /* 18 */
 	-- A mechanic can have a list of certifications
 	SELECT employee_employeeid, skillname, dateearned FROM mechanic m INNER JOIN certification c ON
     	m.EMPLOYEE_EMPLOYEEID = c.MECHANIC_EMPLOYEE_EMPLOYEEID INNER JOIN skill s ON s.SKILLNAME = c.SKILL_SKILLNAME;
+
+
 
 	-- Technicians receive a Salary, Mechanics get paid on an hourly basis
 	SELECT e.EMPLOYEEID, 'Salary' AS TypeOfPayment FROM employee e inner join technician t ON
@@ -47,4 +151,11 @@ SELECT itemname FROM maintenanceitem WHERE package_packageid IS NULL;
     
 	-- Estimated Yearly Mileage must be 18 year-olds or older
 	SELECT estimatedmileage FROM vehicle;
+
+
+
+        -- A mechanic's hourly rate cannot be less than $10.00 
+        SELECT * from mechanic where hourlyRate >= 10.00;
+
+
 
